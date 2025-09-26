4e8474cd36a58c761bfc39137f31f343d4d50040
lisa-gilot <lisa.gilot@edu.esiee.fr>
2025-09-26 08:12:40 +0000

    code palindrome

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-#### Fonction secondaire
-
+"""
+Code permettant de déterminer si une chaine de caractères est un palindrome
+"""
 
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
-
-#### Fonction principale
-
+    """
+    Détermine si p(la chaine de caractères à tester) est un palindrome
+    Renvoie un booléen indiquant True si p est un palindrome
+    """
+    p = p.replace(" ","").lower()  #pour enlever les espaces et majuscule
+    for t in ",;:/.?!*-_([])='":
+        p = p.replace(t, "")
+    for t in "éèêë":
+        p = p.replace(t, "e")
+    p = p.replace("à","a").replace("ç","c").replace("ô","o").replace("ù","u")
+    pi = p[::-1]
+    return pi == p
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    '''
+    Réalise le test de la fonction ispalindrome pour certaines chaines
+    de caractères
+    '''
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
